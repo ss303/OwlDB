da524a9f7f73b46982dcc037d7ae420991ef1d45
Sunny Sun <ss303@rice.edu>
2025-06-23 23:17:53 -0400

    Delete owldb-p1group35-main directory

--- owldb-p1group35-main/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/RICE-COMP318-FALL24/owldb-p1group35
-
-go 1.23.0
-
-require github.com/santhosh-tekuri/jsonschema/v5 v5.3.1
--- owldb-p1group35-main/storage/childNode.go
+++
@@ -1,7 +0,0 @@
-package storage
-
-type IChildNode interface {
-	GetChild(string) (IChildNode, error)
-	Handle(RequestPack) (content any, status status)
-	GetPath() string
-}
